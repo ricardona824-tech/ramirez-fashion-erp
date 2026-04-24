b27327899d605297fe199b19de3849c832f8ca81
Tecnofactor <ricardo.cardona@tecnofactor.com>
2026-04-24 15:30:12 -0500

    Corrigiendo STATIC_ROOT para Render

--- core/settings.py
+++ core/settings.py
@@ -116,6 +116,7 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
