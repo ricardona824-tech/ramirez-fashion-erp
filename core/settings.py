6da23fe9918c79a6a7c847bd99f7d075e2cf6711
ricardona824-tech <ricardona824@gmail.com>
2026-04-28 19:15:07 -0500

    Mejorando vista para celulares y apagando modo oscuro

--- core/settings.py
+++ core/settings.py
@@ -89,10 +89,7 @@ DATABASES = {
 }
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)  # type: ignore
 
 
 # Password validation
